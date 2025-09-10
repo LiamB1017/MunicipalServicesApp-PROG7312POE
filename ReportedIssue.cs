339ec909721d392176d106522daee1c912f25545
Liam Brown <131519232+LiamB1017@users.noreply.github.com>
2025-09-10 21:05:21 +0200

    Added the data storage class

---
+++ ReportedIssue.cs
@@ -0,0 +1,23 @@
+﻿using MunicipalServicesApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MunicipalServicesApp
+{
+    public class ReportedIssue
+    {
+        public string Location { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Attachment { get; set; }
+    }
+
+}
+
+public static class IssueStorage
+{
+    public static List<ReportedIssue> ReportedIssues = new List<ReportedIssue>();
+}
