15367e186af1404848c3c08a777a4eb2a4045cb0
Liam Brown <131519232+LiamB1017@users.noreply.github.com>
2025-10-15 17:43:01 +0200

    Added all lists, buttons etc. to LocalEventsForm.cs

---
+++ Forms/LocalEventsForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace MunicipalServicesApp.Forms
+{
+    partial class LocalEventsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.lstEvents = new System.Windows.Forms.ListBox();
+            this.btnRecommend = new System.Windows.Forms.Button();
+            this.lblRecommendations = new System.Windows.Forms.Label();
+            this.lstRecommendations = new System.Windows.Forms.ListBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(451, 58);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(192, 15);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Local Events and Announcements";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(454, 147);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(213, 122);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Items.AddRange(new object[] {
+            "Community",
+            "Sports",
+            "Culture",
+            "Education",
+            "Health"});
+            this.cmbCategory.Location = new System.Drawing.Point(166, 221);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategory.TabIndex = 3;
+            // 
+            // lstEvents
+            // 
+            this.lstEvents.FormattingEnabled = true;
+            this.lstEvents.Location = new System.Drawing.Point(425, 197);
+            this.lstEvents.Name = "lstEvents";
+            this.lstEvents.Size = new System.Drawing.Size(120, 95);
+            this.lstEvents.TabIndex = 4;
+            // 
+            // btnRecommend
+            // 
+            this.btnRecommend.Location = new System.Drawing.Point(435, 315);
+            this.btnRecommend.Name = "btnRecommend";
+            this.btnRecommend.Size = new System.Drawing.Size(75, 23);
+            this.btnRecommend.TabIndex = 5;
+            this.btnRecommend.Text = "Show Recommendations";
+            this.btnRecommend.UseVisualStyleBackColor = true;
+            // 
+            // lblRecommendations
+            // 
+            this.lblRecommendations.AutoSize = true;
+            this.lblRecommendations.Location = new System.Drawing.Point(435, 364);
+            this.lblRecommendations.Name = "lblRecommendations";
+            this.lblRecommendations.Size = new System.Drawing.Size(115, 15);
+            this.lblRecommendations.TabIndex = 6;
+            this.lblRecommendations.Text = "Recommendations:";
+            // 
+            // lstRecommendations
+            // 
+            this.lstRecommendations.FormattingEnabled = true;
+            this.lstRecommendations.Location = new System.Drawing.Point(626, 188);
+            this.lstRecommendations.Name = "lstRecommendations";
+            this.lstRecommendations.Size = new System.Drawing.Size(120, 95);
+            this.lstRecommendations.TabIndex = 7;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(438, 401);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 8;
+            this.btnBack.Text = "Back to Main Menu";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // LocalEventsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.lstRecommendations);
+            this.Controls.Add(this.lblRecommendations);
+            this.Controls.Add(this.btnRecommend);
+            this.Controls.Add(this.lstEvents);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "LocalEventsForm";
+            this.Text = "LocalEventsForm";
+            this.Load += new System.EventHandler(this.LocalEventsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.ListBox lstEvents;
+        private System.Windows.Forms.Button btnRecommend;
+        private System.Windows.Forms.Label lblRecommendations;
+        private System.Windows.Forms.ListBox lstRecommendations;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
